2e7621ef6c5ed86a695a51fb4ee53250290de86e
Lars Vierbergen <lars.vierbergen@xenit.eu>
2021-12-03 17:21:11 +0100

    Ensure that running containers themselves are not persisted when a new dind container is created

--- nuntio-app/src/test/java/be/vbgn/nuntio/integtest/containers/DindContainer.java
+++ nuntio-app/src/test/java/be/vbgn/nuntio/integtest/containers/DindContainer.java
@@ -4,52 +4,61 @@ import be.vbgn.nuntio.integtest.util.SimpleContainerModifier;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateVolumeResponse;
 import com.github.dockerjava.api.command.InspectContainerResponse;
-import com.github.dockerjava.api.model.BindOptions;
 import com.github.dockerjava.api.model.HealthCheck;
-import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.Mount;
 import com.github.dockerjava.api.model.MountType;
-import com.github.dockerjava.api.model.Volume;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
-import java.io.File;
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.concurrent.atomic.AtomicInteger;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.junit.rules.TemporaryFolder;
-import org.junit.runner.Description;
+import org.springframework.lang.Nullable;
 import org.testcontainers.DockerClientFactory;
-import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.ResourceReaper;
 
 @Getter
 @Setter
 public class DindContainer extends GenericContainer<DindContainer> {
     private int daemonPort = 2375;
-    private static final DindContainerSharedVolume sharedVolume = new DindContainerSharedVolume();
+    private static final ManagedVolume sharedVolume = new ManagedVolume();
+    private final ManagedVolume containerVolume = new ManagedVolume();
 
-    private static class DindContainerSharedVolume {
+    private static class ManagedVolume {
+        @Nullable
+        private DockerClient dockerClient;
+        @Nullable
         private CreateVolumeResponse createVolumeResponse;
 
         public CreateVolumeResponse create(DockerClient dockerClient) {
             if(createVolumeResponse == null) {
+                this.dockerClient = dockerClient;
                 createVolumeResponse = dockerClient.createVolumeCmd()
                         // Mark with testcontainer labels so ryuk will get rid of it after tests have finished
                         .withLabels(DockerClientFactory.DEFAULT_LABELS)
                         .exec();
             }
             return createVolumeResponse;
+        }
+
+        public Mount getMount(DockerClient dockerClient) {
+            CreateVolumeResponse createdVolume = create(dockerClient);
+
+            return new Mount()
+                    .withType(MountType.VOLUME)
+                    .withSource(createdVolume.getName());
+        }
+
+        public void destroy() {
+            if(createVolumeResponse != null) {
+                dockerClient.removeVolumeCmd(createVolumeResponse.getName()).exec();
+                createVolumeResponse = null;
+                dockerClient = null;
+            }
         }
 
     }
@@ -79,13 +88,15 @@ public class DindContainer extends GenericContainer<DindContainer> {
         );
         addExposedPort(getDaemonPort());
 
-        CreateVolumeResponse createdVolume = sharedVolume.create(getDockerClient());
+        withCreateContainerCmdModifier(
+            SimpleContainerModifier.withMount(sharedVolume.getMount(getDockerClient()).withTarget("/var/lib/docker"))
+            .andThen(SimpleContainerModifier.withMount(containerVolume.getMount(getDockerClient()).withTarget("/var/lib/docker/containers")))
+        );
+    }
 
-        withCreateContainerCmdModifier(SimpleContainerModifier.withMount(new Mount()
-                .withType(MountType.VOLUME)
-                .withSource(createdVolume.getName())
-                .withTarget("/var/lib/docker")
-        ));
+    @Override
+    protected void containerIsStopped(InspectContainerResponse containerInfo) {
+        containerVolume.destroy();
     }
 
     @Override
